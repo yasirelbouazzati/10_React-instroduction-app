8897eaf3bdf66ab51b57d989a0e9f70d46a4681f
yasirelbouazzati <yasir12bouazzati@gmail.com>
2024-01-30 18:41:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const name = "yasir";
+
+  return (
+    <>
+      <h1>{`${name}`}</h1>
+    </>
+  );
+}
+
+export default App
