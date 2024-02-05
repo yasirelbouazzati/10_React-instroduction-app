e290eaa51c9cf60b940cf0c2f208c787522acc8a
yasirelbouazzati <yasir12bouazzati@gmail.com>
2024-02-05 19:49:08 +0100

    App.jsx fixed

--- src/App.jsx
+++ src/App.jsx
@@ -10,31 +10,16 @@ const App = () => {
   const exercises2 = 7;
   const part3 = "State of a component";
   const exercises3 = 14;
-const part_01 = {
-  part: "Fundamentals of React",
-  exercises: 10,
-};
 
-const part_02 = {
-  part: "Using props to pass data",
-  exercises: 7,
-};
-
-const part_03 = {
-  part: "State of a component",
-  exercises: 14,
-};
-
-const courseData = [part_01, part_02, part_03];
   return (
     <div>
       <Header course={course} />
       <Content
-        courseData={part_01}
+        part1={part1}
         exercises1={exercises1}
-        courseData={part_02}
+        part2={part2}
         exercises2={exercises2}
-        courseData={part_03}
+        part3={part3}
         exercises3={exercises3}
       />
       <Total
